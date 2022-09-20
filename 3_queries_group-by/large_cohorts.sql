654ec8cc3021317d8e3983a9b5a481271069a7b5
Hani Siddiqui <hani.siddiqui@gmail.com>
2022-09-20 20:41:28 +0000

    Add more queries using GROUP BY and HAVING

---
+++ 3_queries_group-by/large_cohorts.sql
@@ -0,0 +1,7 @@
+SELECT cohorts.name as cohort_name, COUNT(students.name) as student_count
+FROM cohorts
+JOIN students
+ON cohorts.id = students.cohort_id
+GROUP BY cohorts.name
+HAVING COUNT(students.name) >= 18
+ORDER BY COUNT(students.name);
